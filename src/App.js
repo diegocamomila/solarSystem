fb824555f48a6684e5691415dd9395c15c465905
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-12-24 10:31:08 -0300

    iniciado requisits 1, 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Header from './Header';
+import SolarSystem from './components/SolarSystem';
+// import PlanetCard from './components/PlanetCard';
+// import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    // return (<p>Sistema Solar</p>);
+    return (
+      <spam>
+        <Header />
+        <SolarSystem />
+        {/* <section data-testid="planet-name">
+          <PlanetCard />
+        </section> */}
+      </spam>
+    );
   }
 }
 
